ebc47d3ee3d02d1ea93be429ccc13ea6c5610fa6
a.koshchiy <a.koshchiy@pixonic.com>
2026-09-10 20:32:42 +0300

    PIX-4185: qs integration fixes

--- src/storage/clickhouse_transaction.cpp
+++ src/storage/clickhouse_transaction.cpp
@@ -6,13 +6,13 @@
 namespace duckdb {
 
 ClickhouseTransaction::ClickhouseTransaction(Catalog &catalog, TransactionManager &manager, ClientContext &context)
-    : Transaction(manager, context), client(catalog.Cast<ClickhouseCatalog>().client_options, /* TODO config? */ 10) {
+    : Transaction(manager, context), client_options(catalog.Cast<ClickhouseCatalog>().client_options) {
 }
 
 ClickhouseTransaction::~ClickhouseTransaction() = default;
 
-ClickhouseClient &ClickhouseTransaction::GetClient() {
-	return client;
+unique_ptr<ClickhouseClient> ClickhouseTransaction::NewClient() {
+	return make_uniq<ClickhouseClient>(client_options, 10);
 }
 
 ClickhouseTransaction &ClickhouseTransaction::Get(ClientContext &context, Catalog &catalog) {
